41a69bb1cf81bee289318ed0111585ad7630ec7a
Edwin Figueroa <59811869+edwinfigueroa15@users.noreply.github.com>
2024-03-21 14:11:56 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config({ path : 'variables.env' })
+require('dotenv').config({ path : '.env' })
 
 const sequelize = new Sequelize(process.env.BD_NOMBRE, process.env.BD_USER, process.env.BD_PASS, {
   host: process.env.BD_HOST,
